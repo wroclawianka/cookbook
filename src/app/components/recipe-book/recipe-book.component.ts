cf17c105d2c36b45e1367887257ef2eb2de7617a
wroclawianka <dorota.zelga@gmail.com>
2018-05-20 19:47:24 +0200

    selected recipe displayed

--- src/app/components/recipe-book/recipe-book.component.ts
+++ src/app/components/recipe-book/recipe-book.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipe-book',
   templateUrl: './recipe-book.component.html',
   styleUrls: ['./recipe-book.component.css']
 })
-export class RecipeBookComponent implements OnInit {
+export class RecipeBookComponent {
+  @Input() selectedRecipe: Recipe;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  recipeSelected(recipe : Recipe){
+    this.selectedRecipe = recipe;
   }
 
 }
--- src/app/components/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipies: Recipe[] = [
     new Recipe('Pierogi ruskie',
     `filled dumplings of Central European origin
@@ -26,4 +28,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
